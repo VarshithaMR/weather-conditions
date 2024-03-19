9982acf087082398bc4a9d09dff9f9cff56168d8
Varshitha M R <harman.hm.m@mercedes-benz.com>
2024-03-19 12:05:03 +0530

    TestCases : Here-Maps Client Test cases

--- service/providers/heremaps/here-maps.go
+++ service/providers/heremaps/here-maps.go
@@ -1,6 +1,8 @@
 package heremaps
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/go-resty/resty/v2"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
@@ -27,16 +29,26 @@ type coordinatesRequest struct {
 }
 
 func (c *coordinatesRequest) GetCoordinates(query string) (*models.CoordinatesResponse, error) {
-	url := c.httpClient.BaseURL + hereEndpoint
+	baseUrl := c.httpClient.BaseURL
 	response, err := c.httpClient.R().
 		SetHeader(contentType, appType).
 		SetQueryParam(paramQuery, query).
 		SetResult(models.CoordinatesResponse{}).
-		Get(url)
+		Get(baseUrl + hereEndpoint)
 
 	if err != nil {
 		log.Warnf("❌ HereMaps API error: %s", err)
 		return nil, err
+	}
+
+	// for mocking- test cases
+	if baseUrl == mockUrl {
+		var res models.CoordinatesResponse
+		err = json.Unmarshal(response.Body(), &res)
+		if err != nil {
+			fmt.Println("Testcase : Unmarshal err", err)
+		}
+		return &res, nil
 	}
 
 	return response.Result().(*models.CoordinatesResponse), nil
--- service/providers/heremaps/here-maps_test.go
+++ service/providers/heremaps/here-maps_test.go
@@ -1,10 +1,23 @@
 package heremaps
 
 import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 
+	"github.com/go-resty/resty/v2"
+	"github.com/jarcoal/httpmock"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
+)
+
+const (
+	mockPath                  = "https://mockurl.com/v1/geocode"
+	mockResponse              = "test-util/here-maps-coordinates.json"
+	mockResponseNoCoordinates = "test-util/here-maps-no-coordinates.json"
 )
 
 func TestNewHereMapsClient(test *testing.T) {
@@ -40,9 +53,67 @@ func TestNewHereMapsClient(test *testing.T) {
 	}
 }
 func TestGetCoordinates(test *testing.T) {
+	restyClient := resty.New()
+	jsonResponse, err := os.ReadFile(mockResponse)
+	if err != nil {
+		fmt.Println("Error reading JSON file:", err)
+		return
+	}
 
+	httpmock.ActivateNonDefault(restyClient.GetClient())
+	defer httpmock.DeactivateAndReset()
+
+	recorder := httptest.NewRecorder()
+	reader := bytes.NewReader(jsonResponse)
+	x, err := recorder.Body.ReadFrom(reader)
+	fmt.Println(x)
+	if err != nil {
+		test.Fatal(err)
+	}
+	resp := recorder.Result()
+
+	httpmock.RegisterResponder(http.MethodGet, mockPath, httpmock.ResponderFromResponse(resp))
+
+	mockHereMapsClient := HereMapsMockClient(restyClient)
+	result, err := mockHereMapsClient.GetCoordinates("Berlin")
+	fmt.Println(result)
+
+	// Assertions
+	assert.NoError(test, err)
+	assert.NotNil(test, result)
+	assert.NotNil(test, result.Items)
+	assert.NotNil(test, result.Items[0].Position)
+	assert.NotNil(test, result.Items[0].Position.Longitude)
+	assert.NotNil(test, result.Items[0].Position.Latitude)
 }
 
 func TestGetCoordinatesEmpty(test *testing.T) {
+	restyClient := resty.New()
+	jsonResponse, err := os.ReadFile(mockResponseNoCoordinates)
+	if err != nil {
+		fmt.Println("Error reading JSON file:", err)
+		return
+	}
 
+	httpmock.ActivateNonDefault(restyClient.GetClient())
+	defer httpmock.DeactivateAndReset()
+
+	recorder := httptest.NewRecorder()
+	reader := bytes.NewReader(jsonResponse)
+	_, err = recorder.Body.ReadFrom(reader)
+	if err != nil {
+		test.Fatal(err)
+	}
+	resp := recorder.Result()
+
+	httpmock.RegisterResponder(http.MethodGet, mockPath, httpmock.ResponderFromResponse(resp))
+
+	mockHereMapsClient := HereMapsMockClient(restyClient)
+	result, err := mockHereMapsClient.GetCoordinates("Berlin")
+	fmt.Println(result)
+
+	// Assertions
+	assert.NoError(test, err)
+	assert.NotNil(test, result)
+	assert.Nil(test, result.Items)
 }
